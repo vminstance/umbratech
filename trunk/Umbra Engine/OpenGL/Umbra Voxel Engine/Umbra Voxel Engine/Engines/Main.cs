873f6132a483ea6fd045372b8f2a2ba2becb4c5e
azzi777 <azzi777@358bbac1-c2ed-3386-8ec3-da06f2d79d19>
2011-08-05 18:20:23 +0000

    Renamed the OpenGL repo

---
+++ trunk/Umbra Engine/OpenGL/Umbra Voxel Engine/Umbra Voxel Engine/Engines/Main.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using OpenTK;
+using OpenTK.Audio;
+using OpenTK.Input;
+using OpenTK.Graphics;
+using OpenTK.Platform;
+using OpenTK.Audio.OpenAL;
+using OpenTK.Graphics.OpenGL;
+using Umbra.Engines;
+using Umbra.Utilities;
+using Umbra.Structures;
+using Umbra.Definitions;
+using Umbra.Implementations;
+using Umbra.Structures.Geometry;
+using Umbra.Definitions.Globals;
+using Console = Umbra.Implementations.Graphics.Console;
+
+namespace Umbra.Engines
+{
+    public class Main : GameWindow
+    {
+        List<Engine> Engines;
+
+        public Main(GraphicsMode mode, string title, GameWindowFlags flags)
+            : base((int)Constants.Graphics.ScreenResolution.X, (int)Constants.Graphics.ScreenResolution.Y, mode, title, flags)
+        {
+            Engines = new List<Engine>();
+            Constants.SetupEngines(this);
+        }
+
+        public void AddEngine(Engine engine)
+        {
+            Engines.Add(engine);
+            engine.SetGame(this);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            foreach (Engine engine in Engines)
+            {
+                engine.Initialize(e);
+            }
+
+            Variables.Game.IsInitialized = true;
+            Variables.Game.IsActive = true;
+            base.OnLoad(e);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+
+            foreach (Engine engine in Engines)
+            {
+                engine.Update(e);
+            }
+
+            base.OnUpdateFrame(e);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.ClearColor(Variables.Graphics.ScreenClearColor);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            foreach (Engine engine in Engines)
+            {
+                engine.Render(e);
+            }
+
+            SwapBuffers();
+
+            base.OnRenderFrame(e);
+        }
+    }
+}
